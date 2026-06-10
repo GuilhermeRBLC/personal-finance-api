c577ea059ec565f2696b6a8a7cf9a23ff31118b9
Guilherme <guilhermerblc@gmail.com>
2026-06-10 01:44:10 -0300

    fix: Flyway migrations run

--- src/main/java/com/github/guilhermerblc/personalfinanceapi/config/CorsConfig.java
+++ src/main/java/com/github/guilhermerblc/personalfinanceapi/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.github.guilhermerblc.personalfinanceapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${api.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200") // Permite o Angular
+                        .allowedOrigins(allowedOrigins) // Permite o Angular
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Permite envio de cookies/tokens
